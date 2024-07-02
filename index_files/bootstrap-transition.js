df039c3f9652e06748955bcd58303740939a3cb3
dyepifanov <dmytro.epifanov@boostylabs.com>
2024-07-02 09:32:18 +0300

    First Website version

---
+++ index_files/bootstrap-transition.js
@@ -0,0 +1,95 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* ===================================================
+ * bootstrap-transition.js v2.3.0
+ * http://twitter.github.com/bootstrap/javascript.html#transitions
+ * ===================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+
+!function ($) {
+
+  "use strict"; // jshint ;_;
+
+
+  /* CSS TRANSITION SUPPORT (http://www.modernizr.com/)
+   * ======================================================= */
+
+  $(function () {
+
+    $.support.transition = (function () {
+
+      var transitionEnd = (function () {
+
+        var el = document.createElement('bootstrap')
+          , transEndEventNames = {
+               'WebkitTransition' : 'webkitTransitionEnd'
+            ,  'MozTransition'    : 'transitionend'
+            ,  'OTransition'      : 'oTransitionEnd otransitionend'
+            ,  'transition'       : 'transitionend'
+            }
+          , name
+
+        for (name in transEndEventNames){
+          if (el.style[name] !== undefined) {
+            return transEndEventNames[name]
+          }
+        }
+
+      }())
+
+      return transitionEnd && {
+        end: transitionEnd
+      }
+
+    })()
+
+  })
+
+}(window.jQuery);
+
+}
+/*
+     FILE ARCHIVED ON 21:41:45 May 12, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:12:38 Jun 23, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.832
+  exclusion.robots: 0.094
+  exclusion.robots.policy: 0.08
+  esindex: 0.011
+  cdx.remote: 6.949
+  LoadShardBlock: 85.083 (3)
+  PetaboxLoader3.datanode: 70.923 (5)
+  PetaboxLoader3.resolve: 105.688 (2)
+  load_resource: 100.532
+  loaddict: 17.058
+*/
---
+++ index_files/ru.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+a2a_localize={Share:"&#1056;&#1077;&#1089;&#1091;&#1088;&#1089;",Save:"&#1057;&#1086;&#1093;&#1088;&#1072;&#1085;&#1080;",Subscribe:"&#1055;&#1086;&#1076;&#1087;&#1080;&#1096;&#1080;&#1089;&#1100;",Email:"E-mail",Bookmark:"&#1042; &#1079;&#1072;&#1082;&#1083;&#1072;&#1076;&#1082;&#1080;!",ShowAll:"&#1055;&#1086;&#1082;&#1072;&#1079;&#1072;&#1090;&#1100; &#1074;&#1089;&#1077;",ShowLess:"&#1057;&#1087;&#1088;&#1103;&#1090;&#1072;&#1090;&#1100;",FindServices:"&#1053;&#1072;&#1081;&#1090;&#1080; &#1089;&#1077;&#1088;&#1074;&#1080;&#1089;(&#1099;)",FindAnyServiceToAddTo:"&#1053;&#1072;&#1081;&#1090;&#1080; &#1089;&#1077;&#1088;&#1074;&#1080;&#1089; &#1076;&#1083;&#1103; &#1076;&#1086;&#1073;&#1072;&#1074;&#1083;&#1077;&#1085;&#1080;&#1103;",PoweredBy:"&#1056;&#1072;&#1073;&#1086;&#1090;&#1072;&#1077;&#1090; &#1085;&#1072;",ShareViaEmail:"&#1056;&#1077;&#1089;&#1091;&#1088;&#1089; &#1085;&#1072; e-mail",SubscribeViaEmail:"&#1055;&#1086;&#1076;&#1087;&#1080;&#1089;&#1072;&#1090;&#1100;&#1089;&#1103; &#1087;&#1086; email",BookmarkInYourBrowser:"&#1044;&#1086;&#1073;&#1072;&#1074;&#1080;&#1090;&#1100; &#1074; &#1048;&#1079;&#1073;&#1088;&#1072;&#1085;&#1085;&#1086;&#1077;",BookmarkInstructions:"&#1078;&#1084;&#1080; Ctrl+D &#1080;&#1083;&#1080; Cmd+D &#1095;&#1090;&#1086;&#1073;&#1099; &#1076;&#1086;&#1073;&#1072;&#1074;&#1080;&#1090;&#1100; &#1089;&#1090;&#1088;&#1072;&#1085;&#1080;&#1094;&#1091;",AddToYourFavorites:"&#1044;&#1086;&#1073;&#1072;&#1074;&#1080;&#1090;&#1100; &#1074; &#1083;&#1102;&#1073;&#1080;&#1084;&#1086;&#1077;",SendFromWebOrProgram:"&#1054;&#1090;&#1087;&#1088;&#1072;&#1074;&#1080;&#1090;&#1100; &#1095;&#1077;&#1088;&#1077;&#1079; e-mail &#1089;&#1077;&#1088;&#1074;&#1080;&#1089;",EmailProgram:"E-mail &#1089;&#1077;&#1088;&#1074;&#1080;&#1089;&#1099;",More:"&#1045;&#1097;&#1077;&#8230;",ThanksForSharing:"&#x421;&#x43F;&#x430;&#x441;&#x438;&#x431;&#x43E;, &#x447;&#x442;&#x43E; &#x43F;&#x43E;&#x434;&#x435;&#x43B;&#x438;&#x43B;&#x441;&#x44F;",ThanksForFollowing:"&#x421;&#x43F;&#x430;&#x441;&#x438;&#x431;&#x43E; &#x437;&#x430; &#x43F;&#x43E;&#x434;&#x43F;&#x438;&#x441;&#x43A;&#x443;"};
+
+}
+/*
+     FILE ARCHIVED ON 00:06:00 May 13, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:13:08 Jun 23, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.841
+  exclusion.robots: 0.131
+  exclusion.robots.policy: 0.118
+  esindex: 0.011
+  cdx.remote: 5.623
+  LoadShardBlock: 42.034 (3)
+  PetaboxLoader3.datanode: 131.984 (5)
+  load_resource: 503.672 (2)
+  PetaboxLoader3.resolve: 354.302 (2)
+*/
